79fe59b4274a23e2ead1bc8d341106fead4df7c0
Valya Tureyskaya <turleo@outlook.com>
2024-01-05 17:24:47 +1000

    🔐 Centering on login page fixed

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -5,37 +5,39 @@
 </style>
 
 <template>
-  <div class="login">
-    <div class="welcome">
-      <h1>Добро пожаловать!</h1>
-      <div id="logo"></div>
-      <a href="" class="button fefu">
-        Войти через ДВФУ
-      </a>
-    </div>
+  <div class="container">
+    <div class="login">
+      <div class="welcome">
+        <h1>Добро пожаловать!</h1>
+        <div id="logo"></div>
+        <a href="" class="button fefu">
+          Войти через ДВФУ
+        </a>
+      </div>
 
-    <div class="sign-in-lines">
-      <div id="line"></div>
-      <p>Или войдите по паролю</p>
-      <div id="line"></div>
-    </div>
+      <div class="sign-in-lines">
+        <div id="line"></div>
+        <p>Или войдите по паролю</p>
+        <div id="line"></div>
+      </div>
 
-    <div class="form">
-      <div class="group text">
-        <label for="email">Адрес электронной почты</label>
-        <input type="email" id="email">
+      <div class="form">
+        <div class="group text">
+          <label for="email">Адрес электронной почты</label>
+          <input type="email" id="email">
+        </div>
+        <div class="group text">
+          <label for="password">Пароль</label>
+          <input type="password" id="password">
+        </div>
+        <div class="group">
+          <input type="submit" value="Войти" class="button">
+        </div>
+        <p class="register-link">
+          или
+          <router-link to="register">зарегистрируйтесь</router-link>
+        </p>
       </div>
-      <div class="group text">
-        <label for="password">Пароль</label>
-        <input type="password" id="password">
-      </div>
-      <div class="group">
-        <input type="submit" value="Войти" class="button">
-      </div>
-      <p class="register-link">
-        или
-        <router-link to="register">зарегистрируйтесь</router-link>
-      </p>
     </div>
   </div>
 </template>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -5,33 +5,35 @@
 </style>
 
 <template>
-  <div class="login register">
-    <div class="welcome">
-      <h1>Зарегистрируйтесь</h1>
-    </div>
-
-
-    <div class="form">
-      <div class="group text">
-        <label for="name">Имя</label>
-        <input type="text" id="name">
+  <div class="container">
+    <div class="login register">
+      <div class="welcome">
+        <h1>Зарегистрируйтесь</h1>
       </div>
-
-      <div class="group text">
-        <label for="email">Адрес электронной почты</label>
-        <input type="email" id="email">
+  
+  
+      <div class="form">
+        <div class="group text">
+          <label for="name">Имя</label>
+          <input type="text" id="name">
+        </div>
+  
+        <div class="group text">
+          <label for="email">Адрес электронной почты</label>
+          <input type="email" id="email">
+        </div>
+        <div class="group text">
+          <label for="password">Пароль</label>
+          <input type="password" id="password">
+        </div>
+        <div class="group">
+          <input type="submit" value="Зарегистрироваться" class="button">
+        </div>
+        <p class="register-link">
+          или
+          <router-link to="login">войдите</router-link>
+        </p>
       </div>
-      <div class="group text">
-        <label for="password">Пароль</label>
-        <input type="password" id="password">
-      </div>
-      <div class="group">
-        <input type="submit" value="Зарегистрироваться" class="button">
-      </div>
-      <p class="register-link">
-        или
-        <router-link to="login">войдите</router-link>
-      </p>
     </div>
   </div>
 </template>
