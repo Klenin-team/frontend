b6011a05807006ce0846078c7c2807dcbc5c35c4
Valya Tureyskaya <turleo@outlook.com>
2024-01-05 11:52:28 +1000

    🎉 Working on frontend is started

---
+++ src/views/LoginView.vue
@@ -0,0 +1,39 @@
+<script setup>
+  import '../assets/login.css'
+</script>
+
+<template>
+  <div class="login">
+    <div class="welcome">
+      <h1>Добро пожаловать!</h1>
+      <div id="logo"></div>
+      <a href="" class="button fefu">
+        Войти через ДВФУ
+      </a>
+    </div>
+
+    <div class="sign-in-lines">
+      <div id="line"></div>
+      <p>Или зайти по паролю</p>
+      <div id="line"></div>
+    </div>
+
+    <div class="form">
+      <div class="group text">
+        <label for="email">Адрес электронной почты</label>
+        <input type="email" id="email">
+      </div>
+      <div class="group text">
+        <label for="password">Пароль</label>
+        <input type="password" id="password">
+      </div>
+      <div class="group">
+        <input type="submit" value="Войти" class="button">
+      </div>
+      <p class="register-link">
+        или
+        <router-link to="register">зарегестрируйтесь</router-link>
+      </p>
+    </div>
+  </div>
+</template>
